14e2d0d9443ed00697b4a978b35270e1d910c1d6
Naix <20989997+GhostNaix@users.noreply.github.com>
2024-01-13 16:06:58 +1100

    Updated Build to use Cuda-toolkit 12
    [+] Updated build to use Cuda-Toolkit 12 instead
    [~] Fixed a bug where Cublas library would fail to load in google collab. (Running Cuda 12)
    [+] Package .o files as well
    [-] Deleted and deprecated koboldcpp_make_libs_only.sh
    [+] Added tar.xz format to release

--- koboldcpp_make_libs_only.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -f "bin/micromamba" ]; then
-	curl -Ls https://anaconda.org/conda-forge/micromamba/1.5.3/download/linux-64/micromamba-1.5.3-0.tar.bz2 | tar -xvj bin/micromamba
-fi
-bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
-bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
-bin/micromamba run -r conda -n linux make clean
-bin/micromamba run -r conda -n linux make -j$((`nproc`+1)) LLAMA_OPENBLAS=1 LLAMA_CLBLAST=1 LLAMA_PORTABLE=1
